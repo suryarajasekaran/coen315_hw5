f09b2fb4c58286f671af7eb7cfcb15b66606e8ed
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-24 04:10:43 -0700

    fix

--- HelloBart/platforms/browser/www/js/searchResults.js
+++ HelloBart/platforms/browser/www/js/searchResults.js
@@ -10,6 +10,37 @@ function loadStationInfo() {
     document.getElementById("to").innerHTML = arrivalStation;
 };
 
+function loadDepartureStationInfo() {
+
+    var getStationInfo = localStorage.getItem("departureStation");
+    if (getStationInfo =='Select'){
+    }
+    else{
+        $.ajax({
+            url: "http://suryarajasekaran.com:8881/station?source="+getStationInfo,
+            type: "GET",
+            //contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            cache: false,
+            success: function (result) {
+                var dataset = result.data;
+                console.log(dataset)
+                document.getElementById("name").innerHTML = dataset.name;
+                document.getElementById("intro").innerHTML = dataset.intro['#cdata-section']
+                document.getElementById("address").innerHTML = dataset.address
+                document.getElementById("city").innerHTML = dataset.city
+                document.getElementById("county").innerHTML = dataset.county
+                document.getElementById("state").innerHTML = dataset.state
+                document.getElementById("zipcode").innerHTML = dataset.zipcode
+            },
+            error: function () {
+                console.log("error");
+            }
+        });
+
+    };
+}
+
 function getLatLngOrigin(station){
     $.ajax({
         url: "http://suryarajasekaran.com:8881/station?source="+station,
@@ -65,6 +96,7 @@ function loadResults() {
         document.getElementById("notification").innerHTML = "Results";
         getLatLngDest(filterArrivalStation);
         getLatLngOrigin(filterDepartureStation);
+        initMap();
         $.ajax({
             url: "http://suryarajasekaran.com:8881/trips?source="+filterDepartureStation+"&dest="+filterArrivalStation,
             type: "GET",
@@ -79,8 +111,9 @@ function loadResults() {
                 for(var i = 0; i < dataset.length; i++) {
                     if (i == 0) {
                         document.getElementById("fare").innerHTML = String(dataset[0]["@fare"]);
-                       document.getElementById("co2emission").innerHTML = String(dataset[0]["@co2"]);
-                       document.getElementById("triptime").innerHTML = String(dataset[0]["@tripTime"]);
+                        document.getElementById("co2emission").innerHTML = String(dataset[0]["@co2"]);
+                        document.getElementById("triptime").innerHTML = String(dataset[0]["@tripTime"]);
+                        timer(dataset[0]["@origTimeMin"]);
                     }
                     var div = document.createElement('div');
                     div.className = "panel panel-default";
@@ -104,7 +137,89 @@ function loadResults() {
     }
 }
 
+function addWidgetFlip(year, month, day, hour, min) {
+    document.getElementById("flipwidget").innerHTML = " ";
+    var eventDate = new Date(year,month,day,hour,min);
+    var datetest = (new Date(eventDate).getTime() - new Date().getTime()) / 1000;
+    if (datetest >0){
+
+    console.log(datetest);
+    var clock = $('.your-clock').FlipClock(datetest, {
+        clockFace: 'DailyCounter',
+        countdown: true
+    });
+    console.log(clock);
+    }
+    else document.getElementById("flipwidget").innerHTML = "Your Train has departed ";
+}
+
+function timer(destTimeHours) {
+    var currentTime = new Date();
+    var day = currentTime.getDate();
+    var month = currentTime.getMonth();
+    var year = currentTime.getFullYear();
+    var hour = convert_to_24h(destTimeHours)[0]
+    var min = convert_to_24h(destTimeHours)[1]
+    addWidgetFlip(year,month,day,hour,min)
+}
+
+function convert_to_24h(time_str) {
+    // Convert a string like 10:05 PM to 24h format, returns like [22,5]
+    var time = time_str.match(/(\d+):(\d+) (\w)/);
+    var hours = Number(time[1]);
+    var minutes = Number(time[2]);
+    var meridian = time[3].toLowerCase();
+
+    if (meridian == 'p' && hours < 12) {
+      hours += 12;
+    }
+    else if (meridian == 'a' && hours == 12) {
+      hours -= 12;
+    }
+    return [hours, minutes];
+};
+
+function initMap() {
+    console.log("init"+lat_org)
+    console.log("init"+lng_org)
+    if (lat_org == null || lat_dest == null || lng_org == null || lng_dest == null){
+        document.getElementById("map").innerHTML = " ";
+   }
+   else{
+   document.getElementById("map").innerHTML = " ";
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: {lat: lat_org, lng: lng_org}
+    });
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  }
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    console.log("calc"+lat_org)
+    console.log("calc"+lng_org)
+    var selectedMode = "TRANSIT";
+    directionsService.route({
+      origin: {lat: lat_org, lng: lng_org},
+      destination: {lat: lat_dest, lng: lng_dest},
+      travelMode: google.maps.TravelMode[selectedMode],
+      transitOptions: {
+        modes: ['RAIL']
+      },
+    }, function(response, status) {
+      if (status == 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+}
+
 window.onload = function() {
   loadStationInfo();
+  loadDepartureStationInfo();
   loadResults();
 };
--- HelloBart/www/js/getStat.js
+++ HelloBart/www/js/getStat.js
@@ -1,33 +1,4 @@
 //JS to get More info about one particuar station
 //document.getElementById("searchSchedules").onclick = function () {
-document.getElementById("departureStation").onchange = function () {
-        var getStationInfo = document.getElementById("departureStation").value;
-        if (getStationInfo =='Select'){
-        document.getElementById("StationInfo").innerHTML = "Select source destination to get more info";
-        }
-        else{
-
-        $.ajax({
-                url: "http://suryarajasekaran.com:8881/station?source="+getStationInfo,
-                type: "GET",
-                //contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                cache: false,
-                success: function (result) {
-                var dataset = result.data;
-                //var StationInfo = document.getElementById("StationInfo").value;
-                //StationInfo.result.data;
-                document.getElementById("StationInfo").innerHTML = "<h1>Information about Departure Station</h1> <br>"+"Name of station :" + dataset.name+ "<br> Address of station : "+ dataset.address+
-                 "<br>City : "+dataset.city+"<br>County : "+dataset.county+ "<br>State : "+dataset.state+"<br>ZipCode : "+dataset.zipcode+ "<br> Platform Info :" + dataset.platform_info + "<br> <br> <b>More Information about the station</b> <br> Intro :" +
-                 dataset.intro['#cdata-section']+ "<br> Food :" +dataset.food['#cdata-section']+ "<br> Shopping :" +dataset.shopping['#cdata-section'] + "<br>Attraction :" + dataset.attraction['#cdata-section']
-                  + "<br>Link : " +dataset.link['#cdata-section'];
-                  //"<br>Routes shown below, <br>North :" + dataset.north_routes['route'] +"<br>South :"+dataset.south_routes['route'] +
-
-                //JSON.stringify(result);
-            },
-    error: function () {
-        console.log("error");
-    }
-});
-};
+document.getElementById("departureStation").onchange =
 };
